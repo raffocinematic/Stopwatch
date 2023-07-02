38743df0f487122e0c6cae3beff95a6c51c8eacc
DESKTOP-KQA1RBO\raffo <raffocinematic@gmail.com>
2023-07-02 12:04:14 +0200

    First and last commit

---
+++ Stopwatch/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Stopwatch stopwatch = new Stopwatch();
+		
+		//funziona con più cronometri contemporaneamente
+		//Stopwatch stopwatch2 = new Stopwatch();
+
+	}
+
+}
